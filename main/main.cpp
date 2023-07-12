bc4d517faca27627e9ac216de27a28ce7c9b49fc
Viktor <v@v-vasilev.ru>
2023-07-12 15:54:10 +0300

    pasted all into main, created debug tasks and ringbufffers

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,117 @@
 
-#include "project_header.h"
+#include "Arduino.h"
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+#include "audio_element.h"
+#include "audio_pipeline.h"
+#include "audio_event_iface.h"
+#include "audio_common.h"
+#include "board.h"
+#include "ringbuf.h"
+#include "esp_peripherals.h"
+#include "i2s_stream.h"
+#include "wav_encoder.h"
+#include "wav_decoder.h"
+#include "audio_idf_version.h"
+#include "BluetoothSerial.h"
+#include "codec2.h"
+#include "LoRa.h"
+#include <ButterworthFilter.h>
+
+
+#define I2S_STREAM_CUSTOM_READ_CFG() {                                          \
+    .type = AUDIO_STREAM_READER,                                                \
+    .i2s_config = {                                                             \
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_RX),      \
+        .sample_rate = 8000,                                                    \
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_8BIT,                            \
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,                           \
+        .communication_format = I2S_COMM_FORMAT_STAND_I2S,                      \
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL2 | ESP_INTR_FLAG_IRAM,          \
+        .dma_buf_count = 3,                                                     \
+        .dma_buf_len = 900,                                                     \
+        .use_apll = true,                                                       \
+        .tx_desc_auto_clear = true,                                             \
+        .fixed_mclk = 0                                                         \
+    },                                                                          \
+    .i2s_port = I2S_NUM_0,                                                      \
+    .use_alc = 0,                                                               \
+    .volume = 0,                                                                \
+    .out_rb_size = I2S_STREAM_RINGBUFFER_SIZE,                                  \
+    .task_stack = I2S_STREAM_TASK_STACK,                                        \
+    .task_core = I2S_STREAM_TASK_CORE,                                          \
+    .task_prio = I2S_STREAM_TASK_PRIO,                                          \
+    .stack_in_ext = false,                                                      \
+    .multi_out_num = 0,                                                         \
+    .uninstall_drv = true,                                                      \
+    .need_expand = false,                                                       \
+    .expand_src_bits = I2S_BITS_PER_SAMPLE_8BIT,                                \
+    .buffer_len = 900,                                                          \
+}
+
+
+#define I2S_STREAM_CUSTOM_WRITE_CFG() {                                         \
+     .type = AUDIO_STREAM_WRITER,                                               \
+    .i2s_config = {                                                             \
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_RX),      \
+        .sample_rate = 8000,                                                    \
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_8BIT,                            \
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,                           \
+        .communication_format = I2S_COMM_FORMAT_STAND_I2S,                      \
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL2 | ESP_INTR_FLAG_IRAM,          \
+        .dma_buf_count = 3,                                                     \
+        .dma_buf_len = 900,                                                     \
+        .use_apll = true,                                                       \
+        .tx_desc_auto_clear = true,                                             \
+        .fixed_mclk = 0                                                         \
+    },                                                                          \
+    .i2s_port = I2S_NUM_1,                                                      \
+    .use_alc = 0,                                                               \
+    .volume = 1,                                                                \
+    .out_rb_size = I2S_STREAM_RINGBUFFER_SIZE,                                  \
+    .task_stack = I2S_STREAM_TASK_STACK,                                        \
+    .task_core = 1,                                                             \
+    .task_prio = I2S_STREAM_TASK_PRIO,                                          \
+    .stack_in_ext = false,                                                      \
+    .multi_out_num = 0,                                                         \
+    .uninstall_drv = true,                                                      \
+    .need_expand = false,                                                       \
+    .expand_src_bits = I2S_BITS_PER_SAMPLE_8BIT,                                \
+    .buffer_len = 900,                                                          \
+}
+
+void print_seconds()
+{
+    while(1)
+    {
+        static int seconds_played = 1;
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        printf(TAG,"System running for %d seconds", seconds_played);
+        seconds_played++;
+    }
+}
+
+void print_samples(int8_t *speech)
+{
+    while(1)
+    {
+    printf("[%d]:\t%d" ,i, speech[i] );
+    }
+}
+
+#define SPEECH_BUFFER_SIZE 160
+#define ENCODE_FRAME_BITS 64
+#define ENCODE_FRAME_BYTES 8
+
+static const char *TAG = "MONITORING";
+struct CODEC2* codec2_state;
+int8_t speech[SPEECH_BUFFER_SIZE];
+TaskHandle_t TaskHandle = NULL;
+void * context_ptr = NULL;
+
 
 extern "C" void app_main()
 {
@@ -27,6 +139,11 @@ extern "C" void app_main()
     i2s_writer = i2s_stream_init(&i2s_write_cfg);
     ESP_LOGI(TAG, "4) Configured I2S stream write");
     
+    ringbuf_handle_t speech_read_buffer = rb_create(SPEECH_BUFFER_SIZE,1);
+    ringbuf_handle_t speech_write_buffer = rb_create(SPEECH_BUFFER_SIZE,1);
+    ringbuf_handle_t enc2_out_frame_bits = rb_create(ENCODE_FRAME_BYTES,1);
+    ringbuf_handle_t dec2_in_frame_bits = rb_create(ENCODE_FRAME_BYTES,1);
+
     initArduino();
     Serial.begin(115200);
     while(!Serial){;}
@@ -40,22 +157,27 @@ extern "C" void app_main()
     ESP_LOGI(TAG, "8) successfully linked together [codec_chip]--> i2s_read--> codec2--> i2s_write --> [codec chip]");
 
     audio_event_iface_cfg_t event_cfg = AUDIO_EVENT_IFACE_DEFAULT_CFG(); // ненважно
-    audio_event_iface_handle_t event = audio_event_iface_init(&event_cfg); 
+    audio_event_iface_handle_t pipeline_event = audio_event_iface_init(&event_cfg);
+    audio_event_iface_handle_t read_event = audio_event_iface_init(&event_cfg);
+    audio_event_iface_handle_t write_event = audio_event_iface_init(&event_cfg);
     ESP_LOGI(TAG, "9) Set up  event listener");
 
     audio_pipeline_set_listener(pipeline, event);
+    audio_element_msg_set_listener(audio_element_handle_t i2s_reader, read_event);
+    audio_element_msg_set_listener(audio_element_handle_t i2s_writer, write_event);
     ESP_LOGI(TAG, "10) Set listener event from pipeline");
 
     audio_pipeline_run(pipeline);
     ESP_LOGI(TAG, "11) Started audio pipeline tasks");
+    xTaskCreatePinnedToCore(print_seconds, "print_seconds", 64, NULL, 10, &TaskHandle, 1);
+    xTaskCreatePinnedToCore(print_samples, "print_samples", 64, NULL, 10, &TaskHandle, 1);
 
     while (1) 
     {
-    audio_event_iface_msg_t msg;
+        audio_event_iface_msg_t msg;
         static int seconds_played = 1;
         vTaskDelay(1000/portTICK_PERIOD_MS);
         ESP_LOGI(TAG,"System running for %d seconds", seconds_played);
-        Serial.println("loop");
         seconds_played++;
     }
     
